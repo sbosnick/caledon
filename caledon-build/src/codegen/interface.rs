fc7e9c72bed8d125259fb5686a1117dc4652b771
Steven Bosnick <sbosnick@sympatico.ca>
2021-01-17 10:52:49 -0500

    refactor(build): extract functions from generate_interface

--- caledon-build/src/codegen/interface.rs
+++ caledon-build/src/codegen/interface.rs
@@ -11,7 +11,7 @@ use std::convert::TryInto;
 use proc_macro2::{Ident, TokenStream};
 
 use crate::model::{Args, Documentation, Event, Interface, Message, Request};
-use quote::quote;
+use quote::{format_ident, quote};
 
 use super::{
     doc::{format_long_doc, format_message_new_doc, format_short_doc},
@@ -19,34 +19,57 @@ use super::{
 };
 
 pub(super) fn generate_interface(interface: &Interface, interface_list: &Ident) -> TokenStream {
-    let enum_entry_ident = interface.enum_entry_ident();
     let interface_ident = interface.interface_ident();
-    let interface_doc = format_long_doc(interface, |name| format!("The {} interface.", name));
     let mod_ident = interface.mod_ident();
     let mod_doc = format!("The messages for the {} interface.", interface.name());
-    let req_doc = format!("The requests for the {} interface.", interface.name());
-    let evt_doc = format!("The events for the {} interface.", interface.name());
-    let new_doc = format!("Create a new {}.", interface_ident);
-    let request_entries = interface.requests().map(generate_request_entry);
     let requests = interface.requests().enumerate().map(generate_request);
+    let events = interface.events().enumerate().map(generate_event);
+    let interface_struct =
+        generate_interface_struct(interface, &interface_ident, &mod_ident, interface_list);
+    let requests_enum = generate_requests_enum(interface, &interface_ident);
+    let events_enum = generate_events_enum(interface, &interface_ident);
+
+    quote! {
+        #interface_struct
+
+        #[doc = #mod_doc]
+        pub mod #mod_ident {
+            use std::convert::TryFrom;
+
+            #[allow(unused_imports)]
+            use std::ffi::CString;
+
+            use crate::core::{FromOpcodeError, Message, MessageList, MessageMaker, ObjectId, OpCode};
+
+            #[allow(unused_imports)]
+            use crate::core::{Decimal, Fd};
+
+            #requests_enum
+
+            #events_enum
+
+            #(#requests)*
+
+            #(#events)*
+        }
+    }
+}
+
+fn generate_interface_struct(
+    interface: &Interface,
+    interface_ident: &Ident,
+    mod_ident: &Ident,
+    interface_list: &Ident,
+) -> TokenStream {
+    let enum_entry_ident = interface.enum_entry_ident();
+    let interface_doc = format_long_doc(interface, |name| format!("The {} interface.", name));
+    let new_doc = format!("Create a new {}.", interface_ident);
     let request_factories = interface
         .requests()
         .map(|r| generate_request_factory(r, &mod_ident));
-    let request_from_opcode_entries = interface
-        .requests()
-        .enumerate()
-        .map(generate_request_from_op_entry);
-    let request_has_fd_entries = interface.requests().enumerate().map(generate_has_fd_entry);
-    let event_entries = interface.events().map(generate_event_entry);
-    let events = interface.events().enumerate().map(generate_event);
     let event_factories = interface
         .events()
         .map(|e| generate_event_factory(e, &mod_ident));
-    let event_from_opcode_entries = interface
-        .events()
-        .enumerate()
-        .map(generate_event_from_op_entry);
-    let event_has_fd_entries = interface.events().enumerate().map(generate_has_fd_entry);
 
     quote! {
         #[doc = #interface_doc]
@@ -88,78 +111,88 @@ pub(super) fn generate_interface(interface: &Interface, interface_list: &Ident) 
                 }
             }
         }
+    }
+}
 
-        #[doc = #mod_doc]
-        pub mod #mod_ident {
-            use std::convert::TryFrom;
+fn generate_requests_enum(interface: &Interface, interface_ident: &Ident) -> TokenStream {
+    let doc = format!("The requests for the {} interface.", interface.name());
+    let entries = interface.requests().map(generate_request_entry);
+    let from_opcode_entries = interface
+        .requests()
+        .enumerate()
+        .map(generate_request_from_op_entry);
+    let has_fd_entries = interface.requests().enumerate().map(generate_has_fd_entry);
+    let messages_ident = format_ident!("Requests");
 
-            #[allow(unused_imports)]
-            use std::ffi::CString;
+    generate_messages_enum(
+        doc,
+        entries,
+        from_opcode_entries,
+        has_fd_entries,
+        &messages_ident,
+        interface_ident,
+    )
+}
 
-            use crate::core::{FromOpcodeError, Message, MessageList, MessageMaker, ObjectId, OpCode};
+fn generate_events_enum(interface: &Interface, interface_ident: &Ident) -> TokenStream {
+    let doc = format!("The events for the {} interface.", interface.name());
+    let entries = interface.events().map(generate_event_entry);
+    let from_opcode_entries = interface
+        .events()
+        .enumerate()
+        .map(generate_event_from_op_entry);
+    let has_fd_entries = interface.events().enumerate().map(generate_has_fd_entry);
+    let messages_ident = format_ident!("Events");
 
-            #[allow(unused_imports)]
-            use crate::core::{Decimal, Fd};
+    generate_messages_enum(
+        doc,
+        entries,
+        from_opcode_entries,
+        has_fd_entries,
+        &messages_ident,
+        interface_ident,
+    )
+}
+fn generate_messages_enum<E, F, H>(
+    doc: String,
+    entries: E,
+    from_opcode_entries: F,
+    has_fd_entries: H,
+    messages_ident: &Ident,
+    interface_ident: &Ident,
+) -> TokenStream
+where
+    E: Iterator<Item = TokenStream>,
+    F: Iterator<Item = TokenStream>,
+    H: Iterator<Item = TokenStream>,
+{
+    quote! {
+        #[doc = #doc]
+        pub enum #messages_ident {
+            #(#entries,)*
+        }
 
-            #[doc = #req_doc]
-            pub enum Requests {
-                #(#request_entries,)*
+        impl MessageList for #messages_ident {
+            type Interface = super::#interface_ident;
+
+            #[allow(unused_variables, unused_mut, clippy::match_single_binding)]
+            fn from_opcode<MM: MessageMaker>(opcode: OpCode, mut maker: MM) -> Result<Self, FromOpcodeError<MM::Error>> {
+                let item = match opcode {
+                    #(#from_opcode_entries)*
+                    _ => return Err(FromOpcodeError::InvalidOpcode{opcode}),
+                };
+
+                #[allow(unreachable_code)]
+                Ok(item)
             }
 
-            impl MessageList for Requests {
-                type Interface = super::#interface_ident;
-
-                #[allow(unused_variables, unused_mut, clippy::match_single_binding)]
-                fn from_opcode<MM: MessageMaker>(opcode: OpCode, mut maker: MM) -> Result<Self, FromOpcodeError<MM::Error>> {
-                    let item = match opcode {
-                        #(#request_from_opcode_entries)*
-                        _ => return Err(FromOpcodeError::InvalidOpcode{opcode}),
-                    };
-
-                    #[allow(unreachable_code)]
-                    Ok(item)
-                }
-
-                #[allow(clippy::match_single_binding, clippy::match_like_matches_macro)]
-                fn has_fd(opcode: OpCode) -> bool {
-                    match opcode {
-                        #(#request_has_fd_entries)*
-                        _ => false,
-                    }
+            #[allow(clippy::match_single_binding, clippy::match_like_matches_macro)]
+            fn has_fd(opcode: OpCode) -> bool {
+                match opcode {
+                    #(#has_fd_entries)*
+                    _ => false,
                 }
             }
-
-            #[doc = #evt_doc]
-            pub enum Events {
-                #(#event_entries,)*
-            }
-
-            impl MessageList for Events {
-                type Interface = super::#interface_ident;
-
-                #[allow(unused_variables, unused_mut, clippy::match_single_binding)]
-                fn from_opcode<MM: MessageMaker>(opcode: OpCode, mut maker: MM) -> Result<Self, FromOpcodeError<MM::Error>> {
-                    let item = match opcode {
-                        #(#event_from_opcode_entries)*
-                        _ => return Err(FromOpcodeError::InvalidOpcode{opcode}),
-                    };
-
-                    #[allow(unreachable_code)]
-                    Ok(item)
-                 }
-
-                #[allow(clippy::match_single_binding, clippy::match_like_matches_macro)]
-                fn has_fd(opcode: OpCode) -> bool {
-                    match opcode {
-                        #(#event_has_fd_entries)*
-                        _ => false,
-                    }
-                }
-             }
-
-            #(#requests)*
-
-            #(#events)*
         }
     }
 }
