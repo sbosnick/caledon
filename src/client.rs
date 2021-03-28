703638fdbe47b419153b779e3e4da281228eab48
Steven Bosnick <sbosnick@sympatico.ca>
2021-03-28 09:35:55 -0400

    feat(client): implement Display::dispatch
    The initial implementation handles transport and protocol errors
    (wl_display::wl_error events) by bubbling them up to the caller of
    dispatch. It silently ignores all other incoming events.

--- src/client.rs
+++ src/client.rs
@@ -12,10 +12,11 @@
 
 use std::{convert::TryInto, error, fmt, iter::FromIterator, marker::PhantomData, sync::Arc};
 
+use crossbeam::atomic::AtomicCell;
 use futures_core::Stream;
 use futures_sink::Sink;
 use futures_util::{future, sink::SinkExt, stream::TryStreamExt};
-use snafu::{IntoError, ResultExt, Snafu};
+use snafu::{IntoError, OptionExt, ResultExt, Snafu};
 
 use crate::{
     core::{
@@ -61,7 +62,7 @@ where
     /// This method resolves with either a fatal error on the channel or if the
     /// stream of event messages reaches the end.
     pub async fn dispatch(&self) -> Result<(), ClientError> {
-        todo!();
+        Ok(self.inner.dispatch().await?)
     }
 }
 
@@ -76,13 +77,17 @@ enum ClientErrorImpl<E: error::Error + 'static> {
     #[snafu(display("Error with the underlying communication channel during the {}", phase))]
     Transport { source: E, phase: ClientPhase },
 
-    #[snafu(display("Fatal protocol error with the Wayland protocol during the {}", phase))]
-    Protocol { phase: ClientPhase },
+    #[snafu(display("Fatal protocol error during the {}: {}", phase, message))]
+    Protocol { phase: ClientPhase, message: String },
+
+    #[snafu(display("Attempt to dispatch the Display a second time."))]
+    MultiDispach,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum ClientPhase {
     InitialHandshake,
+    Dispatch,
 }
 
 impl fmt::Display for ClientPhase {
@@ -90,6 +95,7 @@ impl fmt::Display for ClientPhase {
         use ClientPhase::*;
         match self {
             InitialHandshake => write!(f, "initial handshake"),
+            Dispatch => write!(f, "dipatch phase"),
         }
     }
 }
@@ -98,7 +104,8 @@ struct DisplayImpl<Si, St, WS, E> {
     // TODO: remove this when it is no longer needed
     #[allow(dead_code)]
     registry: Registry,
-    _phantom: PhantomData<(Si, St, WS, E)>,
+    recv: AtomicCell<Option<St>>,
+    _phantom: PhantomData<(Si, WS, E)>,
 }
 
 impl<Si, St, WS, E> DisplayImpl<Si, St, WS, E>
@@ -126,20 +133,38 @@ where
             .try_take_while(|item| future::ok(!is_done(item)))
             .map_err(|e| Transport { phase }.into_error(e))
             .try_fold(Vec::new(), |mut g, i| {
-                extract_global(&i).map_or_else(
-                    || future::err(Protocol { phase }.build()),
-                    |global| {
-                        g.push(global);
-                        future::ok(g)
-                    },
-                )
+                extract_global(&i).map_or_else(handshake_error, |global| {
+                    g.push(global);
+                    future::ok(g)
+                })
             })
             .await?;
 
         Ok(Self {
             registry: Registry::from_iter(globals),
+            recv: AtomicCell::new(Some(recv)),
             _phantom: PhantomData,
         })
+    }
+
+    async fn dispatch(&self) -> Result<(), ClientErrorImpl<E>> {
+        use protocols::{
+            wayland::{wl_display::Events::Error as WlError, Events::WlDisplay},
+            Events::Wayland,
+        };
+        let phase = ClientPhase::Dispatch;
+        let recv = self.recv.take().context(MultiDispach {})?;
+
+        recv.map_err(|e| Transport { phase }.into_error(e))
+            .try_for_each(|event| match event {
+                Wayland(WlDisplay(WlError(e))) => {
+                    let (_, _, m) = e.args();
+                    let message = m.to_string_lossy();
+                    future::err(Protocol { phase, message }.build())
+                }
+                _ => future::ok(()),
+            })
+            .await
     }
 }
 
@@ -211,12 +236,26 @@ fn extract_global(event: &protocols::Events) -> Option<(u32, Global)> {
     }
 }
 
+fn handshake_error<E>() -> future::Ready<Result<Vec<(u32, Global)>, ClientErrorImpl<E>>>
+where
+    E: error::Error + 'static,
+{
+    future::err(
+        Protocol {
+            phase: ClientPhase::InitialHandshake,
+            message: "unexpected event".to_string(),
+        }
+        .build(),
+    )
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
 
     use std::{
+        convert::Infallible,
         ffi::CString,
         sync::{
             atomic::{AtomicU32, Ordering},
@@ -224,12 +263,19 @@ mod tests {
         },
     };
 
+    use assert_matches::assert_matches;
     use futures::{channel::mpsc, prelude::*, stream};
 
     use crate::core::{testutil::new_object_id, ObjectId};
 
     #[derive(Debug, Snafu)]
     struct StubError;
+
+    impl From<Infallible> for StubError {
+        fn from(_: Infallible) -> Self {
+            Self
+        }
+    }
 
     #[derive(Default)]
     struct StubState {
@@ -251,6 +297,36 @@ mod tests {
         fn add_remote_object(&self, _id: ObjectId, _object: protocols::Protocols) {}
 
         fn remove_object(&self, _id: ObjectId) {}
+    }
+
+    type DrainSink =
+        sink::SinkErrInto<sink::Drain<protocols::Requests>, protocols::Requests, StubError>;
+
+    type DispatchDisplay = DisplayImpl<
+        DrainSink,
+        mpsc::Receiver<Result<protocols::Events, StubError>>,
+        StubState,
+        StubError,
+    >;
+
+    async fn new_dispatch_display_impl() -> (
+        DispatchDisplay,
+        mpsc::Sender<Result<protocols::Events, StubError>>,
+    ) {
+        use protocols::{wayland::wl_callback::DoneEvent, Events::Wayland};
+
+        let (mut sender, receivor) = mpsc::channel(100);
+        let drain: DrainSink = sink::drain().sink_err_into();
+        sender
+            .send(Ok(Wayland(DoneEvent::new(new_object_id(3), 0).into())))
+            .await
+            .expect("Can't send DoneEvent");
+
+        let display = DisplayImpl::new(drain, receivor, Default::default())
+            .await
+            .expect("Can't create DisplayImpl");
+
+        (display, sender)
     }
 
     #[tokio::test]
@@ -300,5 +376,40 @@ mod tests {
         let result = DisplayImpl::new(sink::drain().sink_map_err(|_| StubError), recv, state).await;
 
         assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn display_impl_multi_dispatch_is_error() {
+        let (sut, _) = new_dispatch_display_impl().await;
+        let _ = sut.dispatch().await;
+        let result = sut.dispatch().await;
+
+        assert!(result.is_err())
+    }
+
+    #[tokio::test]
+    async fn display_impl_dipatch_error_is_transport_error() {
+        let (sut, mut send) = new_dispatch_display_impl().await;
+        send.send(Err(StubError))
+            .await
+            .expect("Can't send StubError");
+        let result = sut.dispatch().await;
+
+        assert_matches!(result, Err(ClientErrorImpl::<StubError>::Transport { .. }));
+    }
+
+    #[tokio::test]
+    async fn display_impl_dipatch_error_event_is_protocol_error() {
+        use protocols::{wayland::wl_display::ErrorEvent, Events::Wayland};
+        let display_id = new_object_id(1);
+        let message = CString::new("General Protocol Error").expect("can't make message");
+        let event = Wayland(ErrorEvent::new(display_id, display_id, 0, message).into());
+
+        let (sut, mut send) = new_dispatch_display_impl().await;
+        send.send(Ok(event)).await.expect("Can't send ErrorEvent");
+        send.close_channel();
+        let result = sut.dispatch().await;
+
+        assert_matches!(result, Err(ClientErrorImpl::<StubError>::Protocol { .. }));
     }
 }
